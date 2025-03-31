18491a5d6bebcf45587e24fe6933e2fb376bddce
Vsevolod <wsewolod@mail.ru>
2025-03-31 18:36:09 +0300

    Set metadata extension

--- law_rag/documents/__init__.py
+++ law_rag/documents/__init__.py
@@ -1,3 +1,4 @@
+"""This module contains the document functions to create, modify, prepare and do other stuff for document preparation"""
 from . import common
 from . import convert_pdf
 from . import md_parser
--- law_rag/documents/common.py
+++ law_rag/documents/common.py
@@ -61,14 +61,19 @@ def merge_text(texts: List[str]) -> str:
     return full_text
 
 def set_metadata_to_documents(
-    param_name: str, 
-    param_value: Union[int, float, str], 
+    param_name: str | List[str], 
+    param_value: Union[int, float, str] | List[Union[int, float, str]], 
     docs: List[Document]
 ) -> List[Document]:
-    """Set the one specific metadata to all Documents
+    """Set the specific metadata to all Documents
     
-    This function is used for setting a metadata parameter to all Documents in List.
+    This function is used for setting a metadata parameter to all Documents in List.\n
+    Metadata parameter may be a single value or a list of values.\n
+    Note that `param_name` and `param_value` must be a single value or a list of values both, or the function will raise a TypeError.
 
+
+    Example
+    -------
     For example, if the Document contains this pack of metadata:
     ```
     {"source": "path_to_source_document", "number": 45}
@@ -94,10 +99,10 @@ def set_metadata_to_documents(
 
     Arguments
     ---------
-    param_name: str
-        Name of the new metadata parameter
-    param_value: Union[int, float, str]
-        Value of the new metadata parameter
+    param_name: str | List[str]
+        Name (or list of names) of the new metadata parameter
+    param_value: Union[int, float, str] | List[Union[int, float, str]]
+        Value (or list of values) of the new metadata parameter
     docs: List[Document]
         List of Documents that needs new metadata parameter
         
@@ -105,7 +110,33 @@ def set_metadata_to_documents(
     -------
     docs: List[Documents]
         List of Documents with new metadata parameter
+    
+    Raises
+    ------
+    TypeError
+        If the param_name and param_value are not a single value or a list of values simultaneously
     """
-    for doc in docs:
-        doc.metadata[param_name] = param_value
+    # Checks and preparations for single metadata value
+    if type(param_name) is str:
+        if type(param_name) in [int, float, str]:
+            param_name = [param_name]
+            param_value = [param_value]
+        else:
+            raise TypeError(
+                f"""param_name and param_value must be the single values both. 
+                The param_name is a string, but the param_value is a {type(param_value)}, not a Union[int, float, str]."""
+            )
+
+    # Check for list value
+    if type(param_name) is list:
+        if type(param_value) is not list:
+            raise TypeError(
+                f"""param_name and param_value must be the list values both. 
+                The param_name is a list, but the param_value is a {type(param_value)}, not a list."""
+            )
+    
+    # The actual metadata setup
+    for metadata_index, metadata_name in enumerate(param_name):
+        for doc in docs:
+            doc.metadata[metadata_name] = param_value[metadata_index]
     return docs
